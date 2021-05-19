8ed8686a8dcf581ea5ba9d4e992e9ede560f388d
joestandring <git@joestandring.com>
2021-05-19 14:16:06 +0100

    Finish header

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
-import { Menu } from 'antd';
+import {
+  Menu,
+  Row,
+  Col,
+  Typography,
+} from 'antd';
+
+const { Title } = Typography;
 
 function Nav() {
   return (
-    <>
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        selectedKeys={0}
-      >
-        <Menu.Item key="about">About</Menu.Item>
-        <Menu.Item key="projects">Projects</Menu.Item>
-        <Menu.Item key="blog">Blog</Menu.Item>
-      </Menu>
-    </>
+    <Row>
+      <Col flex="auto">
+        <Title
+          level={2}
+          style={{
+            color: '#f2f2f2',
+            marginTop: '10.5px',
+            marginLeft: '10.5px',
+          }}
+        >
+          Joe Standring
+        </Title>
+      </Col>
+
+      <Col>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          selectedKeys="about"
+        >
+          <Menu.Item key="about">About</Menu.Item>
+          <Menu.Item key="projects">Projects</Menu.Item>
+          <Menu.Item key="blog">Blog</Menu.Item>
+          <Menu.Item key="github">GitHub</Menu.Item>
+        </Menu>
+      </Col>
+    </Row>
   );
 }
 
