ece5cb1e85365ee10e4cbc62d3711fc72185378e
joestandring <git@joestandring.com>
2021-09-02 15:05:58 +0100

    Add blog posts

--- src/components/content/Post.jsx
+++ src/components/content/Post.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Spin } from 'antd';
+import { Spin, Typography } from 'antd';
 import Data from '../../data/blog.json';
+
+const { Title, Text } = Typography;
 
 function Post() {
   const [post, setPost] = useState();
@@ -15,8 +17,30 @@ function Post() {
     setPost(GetPost(route));
   }, route);
 
+  // TODO: Fix styling and linkedin route to correct place
   if (post != null) {
-    return post.title;
+    return (
+      <>
+        <Title className="page-title" style={{ paddingTop: '50px' }}>{post.title}</Title>
+        <Title className="page-title" level={3} type="secondary"><i>{post.subtitle}</i></Title>
+        <div className="vert-center">
+          <img
+            src={`/${post.thumb}`}
+            alt={post.name}
+            width="60%"
+          />
+        </div>
+        <div className="vert-center">
+          <a href="google.com">View on LinkedIn</a>
+        </div>
+        <div>
+          <Text type="secondary">{post.date}</Text>
+        </div>
+        <div>
+          <Text>{post.content}</Text>
+        </div>
+      </>
+    );
   // eslint-disable-next-line no-else-return
   } else {
     return (
